dd6f01b8396f0079a54481d2843b6ea89afc26aa
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2021-09-22 12:43:48 +0530

    exercise

--- block-BJaacz/code/index.js
+++ block-BJaacz/code/index.js
@@ -3,14 +3,29 @@
 /*
   Create a function named addTwo which accepts a number, adds 2 to the number and return the new value.
 */
+``;js
+function addTwo(num){
+  num=num+2;
+  return num;
+}
+
 
 /*
   Create a function named addThree which accepts a number, adds 3 to the number and return the new value.
 */
 
+function addThree(num){
+  num=num+3;
+  return num;
+}
+
 /*
   Create a function named addFive which accepts a number, adds 5 to the number and return the new value.
 */
+function addFive(num){
+  num=num+5;
+  return num;
+}
 
 /*
   Create a function named addTwoToArray which accepts:

7845d211e58a3d437655b2831d4c587b31eb1698
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2021-10-14 18:33:01 +0530

    Block-4 updates

--- block-BJaadm/code/index.js
+++ block-BJaadm/code/index.js
@@ -34,49 +34,110 @@ var filteredName=peopleName.filter(
 console.log(filteredName);
 // Log the length of filtered named of people in peopleName that starts with 'A' and 'C'
 var filterAC=peopleName.filter((name) =>
-name.startsWith('A'));
+name.startsWith('A') || name.startsWith('C')).length;
 console.log(filterAC);
 // Log all the filtered male ('M') in persons array
-let filteredMale=peopleSex.filter((sex)=> sex==='M');
-console.log(filteredMale);
+
+for(people of persons){
+  if(people.sex==='M'){
+    console.log(`"Male Names"-${people.name}`);
+  }
+}
 // Log all the filtered female ('F') in persons array
-
+let filteredFemales=[];
+for(people of persons){
+  if(people.sex==='F'){
+    filteredFemales.push(people.name);
+  }
+}
 // Log all the filtered female ('F') whose name starts with 'C' or 'J' in persons array
-
+let femalenameCJ=filteredFemales.filter((name)=> name.startsWith('C')||name.startsWith('J'));
 // Log all the even numbers from peopleGrade array
+let evenNum=peopleGrade.filter((num)=> num%2===0);
 
 // Find the first name that starts with 'J' in persons array and log the object
-
+for(people of persons){
+  if(people.name.startsWith('J')){
+    console.log(people);
+    break;
+  }
+}
 // Find the first name that starts with 'P' in persons array and log the object
-
+for(people of persons){
+  if(people.name.startsWith('P')){
+    console.log(people);
+    break;
+  }
+}
 // Find the first name that starts with 'J', grade is greater than 10 and is a female
-
+for(people of persons){
+  if(people.name.startsWith('J')&& people.grade>10 && people.sex==='F'){
+    console.log(people);
+    break;
+  }
+}
 // Filter all the female from persons array and store in femalePersons array
-
+let femalePersons=[];
+for(people of persons){
+  if(people.sex==='F'){
+    femalePersons.push(people);
+  }
+}
+console.log(femalePersons);
 // Filter all the male from persons array and store in malePersons array
-
+let malePersons=[];
+for(people of persons){
+  if(people.sex==='M'){
+    malePersons.push(people);
+  }
+}
+console.log(malePersons);
 // Find the sum of all grades and store in gradeTotal
-
+let gradeTotal=peopleGrade.reduce((ac,cv)=>ac=ac+cv,0);
+console.log(gradeTotal);
 // Find the average grade
-
+let gradeAvg= gradeTotal/peopleGrade.length;
+console.log(gradeAvg);
 // Find the average grade of male
-
+let maleGrades=[],maleTotal;
+for(males of malePersons){
+  maleGrades.push(males.grade);
+}
+maleTotal=maleGrades.reduce((ac,cv)=>ac=ac+cv,0);
+console.log(maleTotal);
+let maleAvg=maleTotal/maleGrades.length;
+console.log(maleAvg);
 // Find the average grade of female
 
+let femaleGrades=[],FemaleTotal;
+for(females of femalePersons){
+  femaleGrades.push(females.grade);
+}
+FemaleTotal=femaleGrades.reduce((ac,cv)=>ac=ac+cv,0);
+console.log(FemaleTotal);
+let femaleAvg=FemaleTotal/femaleGrades.length;
+console.log(femaleAvg);
+
 // Find the highest grade
-
+ let highest=[...peopleGrade].sort((a,b)=> a-b).pop();
 // Find the highest grade in male
-
+let highestMale=[...maleGrades].sort((a,b)=>a-b).pop();
 // Find the highest grade in female
-
+let highestFemale=[...femaleGrades].sort((a,b)=>a-b).pop();
 // Find the highest grade for people whose name starts with 'J' or 'P'
-
+let nameJorP=persons.filter((person)=>
+person.name.startsWith('J')||
+person.name.startsWith('P'));
+let gradewithJorP=nameJorP.map((person)=> person.grade);
+[...gradewithJorP].sort((a,b)=>a-b).pop();
 // Sort the peopleGrade in ascending order and log the value of peopleGrade. Notice did the elements of peopleGrade got changed?
-
+let ascSort=peopleGrade.sort((a,b)=>a-b);
+// Yes the elements of the peopleGrade changed.
 // Sort the peopleGrade in descending order
-
+let descSort=peopleGrade.sort((a,b)=> b-a);
 // Sort the peopleGrade in descending order this time you have to make sure you don't mutate the original array
-
+let noMutation=[...peopleGrade].sort((a,b)=>b-a);
 // Sort the array peopelName in ascending `ABCD..Za....z`
-
+peopleName.sort();
 // Sort the array peopelName in ascending `ABCD..Za....z`. Make sure not to mutate the array
+[...peopleName].sort();
